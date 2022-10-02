bbf2f7b1b6bd9fa71e36e31488c0d5b390a1f167
your_username <ducdang513@gmail.com>
2022-10-02 22:13:52 +0700

    Refactoring code pagination

---
+++ recipe/lib/list_recipe/list_recipe_screen.dart
@@ -0,0 +1,59 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import '../uitls/const.dart';
+import 'model/results.dart';
+import 'package:dio/dio.dart';
+import 'package:recipe/api/apiclient.dart';
+
+class ListRecipeScreen extends StatefulWidget {
+  const ListRecipeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ListRecipeScreen> createState() => _ListRecipeScreenState();
+}
+
+class _ListRecipeScreenState extends State<ListRecipeScreen> {
+  final PagingController<int, Results> _pagingController = PagingController(
+      firstPageKey: 1);
+
+  void getListRecipes(int page) async {
+    final dio = Dio();
+    dio.options.headers["Authorization"] = Apis.TOKEN;
+    ApiClient(dio)
+        .getListRecipes(page, "")
+        .then((value) {
+      var results = value.results;
+      var isLastPage = results.length < Const.MAX_ITEM;
+      if (isLastPage) {
+        _pagingController.appendLastPage(results);
+      } else {
+        final nextPage = page++;
+        _pagingController.appendPage(results, nextPage);
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _pagingController.addPageRequestListener((pageKey) {
+      getListRecipes(pageKey);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PagedListView(
+        pagingController: _pagingController,
+        builderDelegate: PagedChildBuilderDelegate<Results>(
+            itemBuilder: (context, item, index) => Text(item.title ?? "")
+        ));
+  }
+
+  @override
+  void dispose() {
+    _pagingController.dispose();
+    super.dispose();
+  }
+}
--- recipe/lib/main.dart
+++ recipe/lib/main.dart
@@ -1,11 +1,5 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:recipe/api/apiclient.dart';
-import 'package:dio/dio.dart';
-import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
-import 'package:recipe/list_recipe/model/results.dart';
-import 'package:recipe/uitls/const.dart';
+import 'list_recipe/list_recipe_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -36,46 +30,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final PagingController<int, Results> _pagingController = PagingController(
-      firstPageKey: 1);
-
-  void getListRecipes(int page) async {
-    final dio = Dio();
-    dio.options.headers["Authorization"] = Apis.TOKEN;
-    ApiClient(dio)
-        .getListRecipes(page, "")
-        .then((value) {
-      var results = value.results;
-      var isLastPage = results.length < Const.MAX_ITEM;
-      if (isLastPage) {
-        _pagingController.appendLastPage(results);
-      } else {
-        final nextPage = page++;
-        _pagingController.appendPage(results, nextPage);
-      }
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _pagingController.addPageRequestListener((pageKey) {
-      getListRecipes(pageKey);
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return PagedListView(
-        pagingController: _pagingController,
-        builderDelegate: PagedChildBuilderDelegate<Results>(
-            itemBuilder: (context, item, index) => Text(item.title ?? "")
-        ));
-  }
-
-  @override
-  void dispose() {
-    _pagingController.dispose();
-    super.dispose();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Recipe"), centerTitle: true,),
+      body: const ListRecipeScreen(),
+    );
   }
 }
