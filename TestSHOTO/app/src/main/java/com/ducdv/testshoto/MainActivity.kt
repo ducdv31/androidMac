41f0532e68ba3450105270c55fd85e84bdd86964
ducdv <ducdang513@gmail.com>
2023-09-07 16:10:16 +0700

    Use camera2

--- TestSHOTO/app/src/main/java/com/ducdv/testshoto/MainActivity.kt
+++ TestSHOTO/app/src/main/java/com/ducdv/testshoto/MainActivity.kt
@@ -1,138 +1,83 @@
 package com.ducdv.testshoto
 
-import android.content.Context
+import android.Manifest
 import android.content.pm.PackageManager
-import android.hardware.camera2.CameraAccessException
-import android.hardware.camera2.CameraDevice
-import android.hardware.camera2.CameraManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.annotation.OptIn
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ExperimentalLensFacing
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import android.Manifest
-import android.hardware.Camera
-import android.view.SurfaceHolder
-import com.ducdv.testshoto.databinding.ActivityMainBinding
-import com.ducdv.testshoto.ext.getCameraIdList
-import com.ducdv.testshoto.ui.view.CameraPreview
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
-    private var camera: Camera? = null
+    private val cameraExecutor: ExecutorService by lazy { Executors.newSingleThreadExecutor() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        // Kiểm tra quyền truy cập CAMERA
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.CAMERA
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
+        setContentView(R.layout.activity_main)
+
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
             ActivityCompat.requestPermissions(
                 this,
-                arrayOf(Manifest.permission.CAMERA),
-                CAMERA_PERMISSION_REQUEST_CODE
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
             )
-        } else {
-            // Quyền truy cập CAMERA đã được cấp
-//            openCameraWithId(CAMERA_ID)
-            // Khởi tạo Camera
-            camera = getCameraInstance()
-
-            // Tạo CameraPreview và đặt nó làm nội dung chính của layout
-            val cameraPreview = CameraPreview(this, camera!!)
-            setContentView(cameraPreview)
         }
     }
 
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext,
+            it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Quyền truy cập CAMERA đã được cấp
-                openCameraWithId(CAMERA_ID)
-            } else {
-                // Quyền truy cập CAMERA bị từ chối
-                Log.e("Camera Permission", "Camera permission denied")
+    @OptIn(ExperimentalLensFacing::class) private fun startCamera() {
+        val previewView = findViewById<PreviewView>(R.id.previewView)
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder().build().also {
+                it.setSurfaceProvider(previewView.surfaceProvider)
             }
-        }
-    }
 
-    private fun openCameraWithId(cameraId: String) {
-        val cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
-        try {
-            if (ActivityCompat.checkSelfPermission(
-                    this,
-                    android.Manifest.permission.CAMERA
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                return
+            val cameraSelector = CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build()
+
+
+            val imageAnalyzer = ImageAnalysis.Builder().build().also {
+                it.setAnalyzer(cameraExecutor) { imageProxy ->
+                    // Xử lý ảnh thu được từ camera
+                    imageProxy.close()
+                }
             }
-            cameraManager.openCamera(cameraId, object : CameraDevice.StateCallback() {
-                override fun onOpened(camera: CameraDevice) {
-                    // Camera đã được mở thành công
-                    Log.d("Camera", "Camera opened")
-                    // TODO: Thực hiện các thao tác với camera
-                }
 
-                override fun onDisconnected(camera: CameraDevice) {
-                    // Camera bị mất kết nối
-                    Log.d("Camera", "Camera disconnected")
-                    // TODO: Xử lý khi mất kết nối camera
-                }
+            cameraProvider.unbindAll()
+            cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalyzer)
 
-                override fun onError(camera: CameraDevice, error: Int) {
-                    // Lỗi khi mở camera
-                    Log.e("Camera", "Camera error: $error")
-                    // TODO: Xử lý lỗi mở camera
-                }
-            }, null)
-        } catch (e: CameraAccessException) {
-            e.printStackTrace()
-        }
-        binding.apply {
-            val surfaceHolder = sfView.holder
-            surfaceHolder.addCallback(object : SurfaceHolder.Callback {
-                override fun surfaceCreated(holder: SurfaceHolder) {
-                    // Được gọi khi SurfaceView được tạo
-                    val canvas = holder.lockCanvas()
-                    // Vẽ hình ảnh, video, đồ họa lên canvas
-                    holder.unlockCanvasAndPost(canvas)
-                }
-
-                override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
-                    // Được gọi khi kích thước hoặc định dạng của SurfaceView thay đổi
-                }
-
-                override fun surfaceDestroyed(holder: SurfaceHolder) {
-                    // Được gọi khi SurfaceView bị hủy
-                }
-            })
-        }
+        }, ContextCompat.getMainExecutor(this))
     }
 
-    private fun getCameraInstance(): Camera? {
-        Log.e("TAG", "getCameraInstance: ${baseContext.getCameraIdList()}")
-        return try {
-            Camera.open(2) // Mở camera mặc định
-        } catch (e: Exception) {
-            // Xử lý lỗi
-            null
-        }
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
 
     companion object {
-        private const val CAMERA_ID = "0"
-        private const val CAMERA_PERMISSION_REQUEST_CODE = 100
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     }
 }
--- TestSHOTO/app/src/main/java/com/ducdv/testshoto/ui/view/CameraPreview.kt
+++ TestSHOTO/app/src/main/java/com/ducdv/testshoto/ui/view/CameraPreview.kt
@@ -1,23 +1,104 @@
-package com.ducdv.testshoto.ui.view
-
+import android.Manifest
 import android.content.Context
-import android.hardware.Camera
+import android.content.pm.PackageManager
+import android.graphics.ImageFormat
+import android.hardware.camera2.*
+import android.media.ImageReader
+import android.util.Size
+import android.view.Surface
 import android.view.SurfaceHolder
 import android.view.SurfaceView
+import androidx.core.app.ActivityCompat
+import java.util.*
 
-class CameraPreview(context: Context, private val camera: Camera) : SurfaceView(context), SurfaceHolder.Callback {
+class CameraPreview(context: Context) : SurfaceView(context), SurfaceHolder.Callback {
 
     private val surfaceHolder: SurfaceHolder = holder.apply {
         addCallback(this@CameraPreview)
     }
 
-    override fun surfaceCreated(holder: SurfaceHolder) {
-        try {
-            camera.setPreviewDisplay(holder)
-            camera.startPreview()
-        } catch (e: Exception) {
+    private var cameraDevice: CameraDevice? = null
+    private var captureSession: CameraCaptureSession? = null
+    private var imageReader: ImageReader? = null
+
+    private val stateCallback = object : CameraDevice.StateCallback() {
+        override fun onOpened(camera: CameraDevice) {
+            cameraDevice = camera
+            createCaptureSession()
+        }
+
+        override fun onDisconnected(camera: CameraDevice) {
+            cameraDevice?.close()
+            cameraDevice = null
+        }
+
+        override fun onError(camera: CameraDevice, error: Int) {
+            cameraDevice?.close()
+            cameraDevice = null
+        }
+    }
+
+    private val captureSessionCallback = object : CameraCaptureSession.StateCallback() {
+        override fun onConfigured(session: CameraCaptureSession) {
+            captureSession = session
+            createCaptureRequest()
+        }
+
+        override fun onConfigureFailed(session: CameraCaptureSession) {
             // Xử lý lỗi
         }
+    }
+
+    private val imageReaderCallback = ImageReader.OnImageAvailableListener { reader ->
+        val image = reader.acquireLatestImage()
+        // Xử lý ảnh thu được từ camera
+        image?.close()
+    }
+
+    private fun createCaptureRequest() {
+        val captureRequestBuilder = cameraDevice?.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+        captureRequestBuilder?.addTarget(surfaceHolder.surface)
+
+        // Thực hiện việc cấu hình và bắt đầu xem trước
+        captureSession?.setRepeatingRequest(
+            captureRequestBuilder!!.build(),
+            null,
+            null
+        )
+    }
+
+    private fun createCaptureSession() {
+        val surfaces = listOf(surfaceHolder.surface, imageReader?.surface).filterNotNull()
+
+        cameraDevice?.createCaptureSession(surfaces, captureSessionCallback, null)
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        // Khởi tạo CameraManager
+        val cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+
+        // Lấy danh sách camera
+        val cameraId = cameraManager.cameraIdList.firstOrNull() ?: return
+
+        // Tạo ImageReader để nhận dữ liệu hình ảnh từ camera
+        val imageReaderSize = chooseImageReaderSize(cameraId)
+        imageReader = ImageReader.newInstance(
+            imageReaderSize.width,
+            imageReaderSize.height,
+            ImageFormat.JPEG,
+            1
+        )
+        imageReader?.setOnImageAvailableListener(imageReaderCallback, null)
+
+        // Mở camera
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        cameraManager.openCamera(cameraId, stateCallback, null)
     }
 
     override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
@@ -25,7 +106,19 @@ class CameraPreview(context: Context, private val camera: Camera) : SurfaceView(
     }
 
     override fun surfaceDestroyed(holder: SurfaceHolder) {
-        // Dừng trước khi hủy
-        camera.stopPreview()
+        captureSession?.close()
+        cameraDevice?.close()
+        imageReader?.close()
+    }
+
+    private fun chooseImageReaderSize(cameraId: String): Size {
+        val cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        val characteristics = cameraManager.getCameraCharacteristics(cameraId)
+        val map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
+        val outputSizes = map?.getOutputSizes(SurfaceHolder::class.java)
+
+        // Chọn kích thước ảnh phù hợp từ danh sách kích thước camera hỗ trợ
+        val largestSize = outputSizes
+        return largestSize!!.first()
     }
 }
